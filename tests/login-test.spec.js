d5df489b151f2dc8e898bd2c578459fd01baa470
JankaPenzes <penzesjanka@gmail.com>
2023-11-27 15:53:14 +0200

    - add a beforeEach hook - add two more cases for login

--- tests/login-test.spec.js
+++ tests/login-test.spec.js
@@ -2,12 +2,14 @@ const { test, expect } = require("@playwright/test");
 import { LoginPage } from "../pages/login.page";
 import { AppointmentPage } from "../pages/appointment.page";
 test.describe("Log in", () => {
-  test("Correct username and password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
-    const appointmentPage = new AppointmentPage(page);
+  test.beforeEach(async ({ page }) => {
     await page.goto(
       "https://katalon-demo-cura.herokuapp.com/profile.php#login"
     );
+  });
+  test("Correct username and password", async ({ page }) => {
+    const loginPage = new LoginPage(page);
+    const appointmentPage = new AppointmentPage(page);
     await loginPage.login("John Doe", "ThisIsNotAPassword");
     await page.waitForURL(
       "https://katalon-demo-cura.herokuapp.com/#appointment"
@@ -16,10 +18,21 @@ test.describe("Log in", () => {
   });
   test("Incorrect username and password", async ({ page }) => {
     const loginPage2 = new LoginPage(page);
-    await page.goto(
-      "https://katalon-demo-cura.herokuapp.com/profile.php#login"
-    );
     await loginPage2.login("John", "ThisIsNot");
+    await expect(page.locator(".text-danger")).toHaveText(
+      "Login failed! Please ensure the username and password are valid."
+    );
+  });
+  test("Incorrect username and correct password", async ({ page }) => {
+    const loginPage3 = new LoginPage(page);
+    await loginPage3.login("John", "ThisIsNotAPassword");
+    await expect(page.locator(".text-danger")).toHaveText(
+      "Login failed! Please ensure the username and password are valid."
+    );
+  });
+  test("Correct username and incorrect password", async ({ page }) => {
+    const loginPage4 = new LoginPage(page);
+    await loginPage4.login("John Doe", "ThisIsNot");
     await expect(page.locator(".text-danger")).toHaveText(
       "Login failed! Please ensure the username and password are valid."
     );
