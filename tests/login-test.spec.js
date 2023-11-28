56b35a64f6a7254b11446a17f3bb61c79f9244cc
JankaPenzes <penzesjanka@gmail.com>
2023-11-28 20:16:56 +0200

    - headles: true

--- tests/login-test.spec.js
+++ tests/login-test.spec.js
@@ -2,13 +2,14 @@ const { test, expect } = require("@playwright/test");
 import { LoginPage } from "../pages/login.page";
 import { AppointmentPage } from "../pages/appointment.page";
 test.describe("Log in", () => {
+  let loginPage;
   test.beforeEach(async ({ page }) => {
     await page.goto(
       "https://katalon-demo-cura.herokuapp.com/profile.php#login"
     );
+    loginPage = new LoginPage(page);
   });
   test("Correct username and password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     const appointmentPage = new AppointmentPage(page);
     await loginPage.login("John Doe", "ThisIsNotAPassword");
     await page.waitForURL(
@@ -17,21 +18,18 @@ test.describe("Log in", () => {
     await expect(appointmentPage.makeAppointmentButton).toBeVisible();
   });
   test("Incorrect username and password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     await loginPage.login("John", "ThisIsNot");
     await expect(page.locator(".text-danger")).toHaveText(
       "Login failed! Please ensure the username and password are valid."
     );
   });
   test("Incorrect username and correct password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     await loginPage.login("John", "ThisIsNotAPassword");
     await expect(page.locator(".text-danger")).toHaveText(
       "Login failed! Please ensure the username and password are valid."
     );
   });
   test("Correct username and incorrect password", async ({ page }) => {
-    const loginPage = new LoginPage(page);
     await loginPage.login("John Doe", "ThisIsNot");
     await expect(page.locator(".text-danger")).toHaveText(
       "Login failed! Please ensure the username and password are valid."
