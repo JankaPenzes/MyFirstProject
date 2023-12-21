a8a85bc487d08f11536cbd30a16ac1533b89444f
Janka Pénzes <144586301+JankaPenzes@users.noreply.github.com>
2023-12-21 11:16:07 +0200

    Update login.spec.js
    - all tests modified, comments deleted

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -10,14 +10,6 @@ const test = base.test.extend({
   },
 });
 test.describe("Log in", () => {
-  // let loginPage 
-  // test.beforeEach(async ({ page }) => {
-  //   // await page.goto( 
-  //   //   "https://katalon-demo-cura.herokuapp.com/profile.php#login"
-  //   // );
-  //   loginPage = new LoginPage(page);
-  //   await loginPage.goTo();
-  // });
   test("Correct username and password", async ({ page, loginPage }) => {
     const appointmentPage = new AppointmentPage(page);
     await loginPage.login("John Doe", "ThisIsNotAPassword");
@@ -26,22 +18,22 @@ test.describe("Log in", () => {
     );
     await expect(appointmentPage.makeAppointmentButton).toBeVisible();
   });
-//   test("Incorrect username and password", async ({ page, loginPage }) => {
-//     await loginPage.login("John", "ThisIsNot");
-//     await expect(loginPage.loginError).toHaveText(
-//       "Login failed! Please ensure the username and password are valid."
-//     );
-//   });
-//   test("Incorrect username and correct password", async ({ page, loginPage }) => {
-//     await loginPage.login("John", "ThisIsNotAPassword");
-//     await expect(loginPage.loginError).toHaveText(
-//       "Login failed! Please ensure the username and password are valid."
-//     );
-//   });
-//   test("Correct username and incorrect password", async ({ page, loginPage }) => {
-//     await loginPage.login("John Doe", "ThisIsNot");
-//     await expect(loginPage.loginError).toHaveText(
-//       "Login failed! Please ensure the username and password are valid."
-//     );
-//   });
+  test("Incorrect username and password", async ({ page, loginPage }) => {
+    await loginPage.login("John", "ThisIsNot");
+    await expect(loginPage.loginError).toHaveText(
+      "Login failed! Please ensure the username and password are valid."
+    );
+  });
+  test("Incorrect username and correct password", async ({ page, loginPage }) => {
+    await loginPage.login("John", "ThisIsNotAPassword");
+    await expect(loginPage.loginError).toHaveText(
+      "Login failed! Please ensure the username and password are valid."
+    );
+  });
+  test("Correct username and incorrect password", async ({ page, loginPage }) => {
+    await loginPage.login("John Doe", "ThisIsNot");
+    await expect(loginPage.loginError).toHaveText(
+      "Login failed! Please ensure the username and password are valid."
+    );
+  });
 });
