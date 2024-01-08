84630ee7f23e74d7048641d62444ba364d074525
JankaPenzes <penzesjanka@gmail.com>
2024-01-08 10:28:18 +0200

    - replacing BeforeEach with fixture

--- tests/appointment.spec.js
+++ tests/appointment.spec.js
@@ -1,23 +1,21 @@
-const { test, expect } = require("@playwright/test");
+const base = require("@playwright/test");
 import { LoginPage } from "../pages/login.page";
 import { AppointmentPage } from "../pages/appointment.page";
-test.describe("Make appointment", () => {
-  let loginPage;
-  let appointmentPage;
-  test.beforeEach(async ({ page }) => {
-    await page.goto(
-      "https://katalon-demo-cura.herokuapp.com/profile.php#login"
-    );
-    loginPage = new LoginPage(page);
-    appointmentPage = new AppointmentPage(page);
+const expect = base.expect;
+const test = base.test.extend({
+  loginPage: async ({ page }, use) => {
+    const loginPage = new LoginPage(page);
+    const appointmentPage = new AppointmentPage(page);
+    await loginPage.goTo();
+    await use(loginPage);
     await loginPage.login("John Doe", "ThisIsNotAPassword");
-    await page.waitForURL(
-      "https://katalon-demo-cura.herokuapp.com/#appointment"
-    );
+    await appointmentPage.waitFor();
     await expect(appointmentPage.makeAppointmentButton).toBeVisible();
-  });
-  test("Make appointment with by filling in all fields correctly", async ({
-    page,
+  },
+});
+test.describe("Make appointment", () => {
+  test("Make appointment by filling in all fields correctly", async ({
+    page, loginPage, appointmentPage
   }) => {
     await appointmentPage.appointment(
       "Tokyo CURA Healthcare Center",
